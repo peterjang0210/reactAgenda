0e665bd2af3609f71b71aebb44430cb5d3e58df2
Peter Jang <peter.jang@outlook.com>
2020-08-01 14:59:25 -0400

    adds basic routing to app.js and shell for listpage

---
+++ client/src/components/listPage/ListPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ListPage extends React.Component {
+    state = {};
+
+    render(){
+        return(
+            <div>
+                ListPage
+            </div>
+        )
+    }
+
+}
+
+export default ListPage;
